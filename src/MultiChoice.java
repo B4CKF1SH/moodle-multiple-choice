efa2aaf59c9dc16ca59fb1385887c0e0fd76719e
B4CKF1SH <43198489+B4CKF1SH@users.noreply.github.com>
2020-05-15 16:41:08 +0200

    Documentation

--- src/MultiChoice.java
+++ src/MultiChoice.java
@@ -1,4 +1,10 @@
 
+/**
+ * Class to represent a Moodle MultiChoice item
+ * 
+ * @author MMC Team
+ *
+ */
 public class MultiChoice extends Item {
 	
 	private String[] choices;
@@ -7,6 +13,16 @@ public class MultiChoice extends Item {
 	private int dependId;
 	private String dependChoice;
 	
+	/**
+	 * MultiChoice constructor, used to instantiate levels of a multi-level feedback
+	 * 
+	 * @param id The Id of the Item
+	 * @param description Description of the feedback Choice (shown)
+	 * @param label Label of the feedback Choice (not shown)
+	 * @param choices Possible choices
+	 * @param dependId The dependency Item's Id
+	 * @param dependChoice The depency choice
+	 */
 	public MultiChoice(int id, String description, String label, String[] choices, int dependId, String dependChoice) {
 		super(id);
 		this.description = description;
@@ -14,15 +30,6 @@ public class MultiChoice extends Item {
 		this.choices = choices;
 		this.dependId = dependId;
 		this.dependChoice = dependChoice;
-	}
-	
-	public MultiChoice(int id, String description, String label, String[] choices) {
-		super(id);
-		this.description = description;
-		this.label = label;
-		this.choices = choices;
-		this.dependId = 0;
-		this.dependChoice = "";
 	}
 
 	/**
@@ -61,22 +68,42 @@ public class MultiChoice extends Item {
 		return value;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public String[] getChoices() {
 		return choices;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public int getDependId() {
 		return dependId;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public String getDependChoice() {
 		return dependChoice;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public String getLabel() {
 		return label;
 	}
