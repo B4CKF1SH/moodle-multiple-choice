513a42fc0768c418f7ca069903e1898268e68cdf
B4CKF1SH <43198489+B4CKF1SH@users.noreply.github.com>
2020-05-15 09:40:56 +0200

    Initial File commit

---
+++ src/Quiz.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+	
+	//list of Questions
+	private List<Question> questions = new ArrayList<>();
+	
+	/**
+	 * Removes a question from the Quiz
+	 * @param index The index of the Question to remove
+	 * @return The removed Question
+	 */
+	public Question removeQuestion(int index) {
+		return questions.remove(index);
+	}
+	
+	/**
+	 * Adds a question to the Quiz
+	 * @param q The Question to add
+	 */
+	public void addQuestion(Question q) {
+		questions.add(q);
+	}
+	
+	
+	@Override
+	/**
+	 * Overriding toString() method to create XML
+	 */
+	public String toString() {
+		
+		//XML header
+		String value = "<?xml version=\"1.0\" ?>\r\n";
+		
+		//opening quiz tag
+		value += "<quiz>\r\n";
+		
+		//questions
+		for (Question q : questions) {
+			value += q.toString();
+		}
+		
+		//closing quiz tag
+		value += "</quiz>";
+		
+		return value;
+	}
+}
