33cff03ee96f0ef3cfabcbd18ca8f7b32df65568
B4CKF1SH <43198489+B4CKF1SH@users.noreply.github.com>
2020-05-15 16:40:43 +0200

    Documentation

--- src/Item.java
+++ src/Item.java
@@ -1,11 +1,25 @@
-public class Item {
+/**
+ * Abstract class to represent a Moodle item
+ * 
+ * @author MMC Team
+ *
+ */
+public abstract class Item {
 	
 	private int id;
 	
+	/**
+	 * Simple constructor - does this really need documentation?
+	 * @param id The Id of the Item 
+	 */
 	public Item(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Simple Getter - does this really need documentation?
+	 * @return The Value
+	 */
 	public int getId() {
 		return id;
 	}
